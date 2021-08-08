322ccdc3ee8715943daea313dfddb0fd5c09c4bd
Tatana <burmatova@inbox.ru>
2021-08-08 18:17:09 +0300

    added files

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/p3s5ut0b58o3hymo/branch/master?svg=true)](https://ci.appveyor.com/project/Tatiana-Brener/task9-reporting-allure/branch/master)
--- src/test/java/ru/netology/test/ChangeDateOfCardDeliveryOrderTest.java
+++ src/test/java/ru/netology/test/ChangeDateOfCardDeliveryOrderTest.java
@@ -1,6 +1,10 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 import ru.netology.data.DataForRegistration;
@@ -12,6 +16,16 @@ import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
 
 public class ChangeDateOfCardDeliveryOrderTest {
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @Test
     void shouldSendCardDeliveryFormWhenCityAndNameAreValid () {
